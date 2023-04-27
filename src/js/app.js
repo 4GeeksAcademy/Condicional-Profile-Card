9270198451c5e4b548f80f00d410f43ea3f821f6
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-04-27 10:06:36 +0200

    Cambios de repositorio

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import "../style/index.css";
 
+import "../style/index.css";
+
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
@@ -33,14 +35,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${variables.name ? variables.name : "Nombre"} ${
+    variables.lastname ? variables.lastname : "Apellido"
+  }</h1>
+          <h2>${variables.role ? variables.role : "Profesión"}</h2>
+          <h3>${variables.city ? variables.city : "Ciudad"}, ${
+    variables.country ? variables.country : "País"
+  }</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href=${
+              variables.twitter ? variables.twitter : "Twitter"
+            }><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${
+              variables.github ? variables.github : "Github"
+            }><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin ? variables.linkedin : "Linkedin"
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${
+              variables.instagram ? variables.instagram : "Instagram"
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
